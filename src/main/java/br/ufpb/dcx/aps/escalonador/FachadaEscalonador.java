bf080efe3ee955d5558884c5c44f9e6225b134c9
Beatriz Maciel <56920708+beatrizstar@users.noreply.github.com>
2020-03-20 21:28:21 -0300

    construtores

--- src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -2,39 +2,20 @@ package br.ufpb.dcx.aps.escalonador;
 
 public class FachadaEscalonador {
 	
-	private int tick;
+	private EscalonadorBase escalonadorBase;
+	private CriadorTipo criadorTipo;
+	
+	public String execute(Command command){
+		
+		command.setEscalonador(this.escalonadorBase);
+		return command.executar();
+	}
 
 	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
-	}
-
-	public FachadaEscalonador(TipoEscalonador roundrobin, int quantum) {
-	}
-
-	public String getStatus() {
-		return "Escalonador RoundRobin;Processos: {};Quantum: 3;Tick: " + tick;
-	}
-
-	public void tick() {
-		tick++;
-	}
-
-	public void adicionarProcesso(String nomeProcesso) {
-	}
-
-	public void adicionarProcesso(String nomeProcesso, int prioridade) {
-	}
-
-	public void finalizarProcesso(String nomeProcesso) {
-	}
-
-	public void bloquearProcesso(String nomeProcesso) {
-	}
-
-	public void retomarProcesso(String nomeProcesso) {
 		
-	}
-
-	public void adicionarProcessoTempoFixo(String string, int duracao) {
-		
+		if(tipoEscalonador == null) throw new EscalonadorException();
+			
+		criadorTipo = gerarTipo(tipoEscalonador);
+		escalonadorBase = criadorTipo.criarEscalonador();
 	}
 }
